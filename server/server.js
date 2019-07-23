89a393639b201c43fad7899eaf425fa23bf78b7f
mohsen adelie <mohsen.adelie@gmail.com>
2019-07-23 23:28:09 +0430

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message'); 
+const {generateMessage, generateLocationMessage} = require('./utils/message'); 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -20,14 +20,20 @@ io.on('connection', (socket) => {
     // socket.broadcast.emit from Admin text New user joined
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text)); 
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');    
+        
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
         //     createAt: new Date().getTime()
         // });
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
